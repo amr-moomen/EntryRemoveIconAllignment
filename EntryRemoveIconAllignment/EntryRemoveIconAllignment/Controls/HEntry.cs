22e0cfe6e8f67f2b2ad341e8d42e6b95460acf97
AmrH <AmrH@DEV-AMR>
2019-12-19 10:17:30 +0200

    Add project files.

---
+++ EntryRemoveIconAllignment/EntryRemoveIconAllignment/Controls/HEntry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace EntryRemoveIconAllignment.Controls
+{
+    public class HEntry : Entry
+    {
+
+        public HEntry()
+        {
+            this.ClearButtonVisibility = ClearButtonVisibility.WhileEditing;
+        }
+
+    }
+
+}
---
+++ EntryRemoveIconAllignment/EntryRemoveIconAllignment/MainPageArabic.xaml.cs
@@ -0,0 +1,29 @@
+﻿using EntryRemoveIconAllignment.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EntryRemoveIconAllignment
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPageArabic : ContentPage
+    {
+        public MainPageArabic()
+        {
+            InitializeComponent();
+           // this.FlowDirection = FlowDirection.RightToLeft;
+        }
+
+        private  void ChangeLangugeClicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new NavigationPage(new MainPage());
+        }
+    
+    }
+}
